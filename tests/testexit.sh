af4e57881abbe55ce70817f2a669c75931f926d4
Ammar Daskin <3620717+adaskin@users.noreply.github.com>
2024-03-26 14:35:05 +0300

    Add files via upload

--- tests/testexit.sh
+++ tests/testexit.sh
@@ -2,20 +2,13 @@
 echo "Running tests..."
 echo
 make all
-./myshell > 11.txt &
-pid=$(pgrep myshell)
-if [[ $pid > 0 ]] ; then
-  echo  "exit" > /proc/$pid/fd/0
-  sleep 0.5
-else
-  echo "Fail: exit test failed."
-  exit 1
-fi
+printf "%s\n" "exit" | ./myshell > 11.txt &
 
 pid=$(pgrep myshell)
-if ! $pid; then 
-  kill -9 $pid  
 
+
+if [[ $pid > 0 ]]; then 
+  kill -9 $pid 
   echo "Fail: exit test failed."
   exit 1
 fi
